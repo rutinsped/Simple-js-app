779f5f4f14a375bf2c01029eb6674dc8fdac5513
rutinsped <rutinsped1@gmail.com>
2019-05-20 20:14:08 +0200

    your message

--- appjs/scripts.js
+++ appjs/scripts.js
@@ -1,1 +1,32 @@
-alert('Hello world');
+/* Seting the repository for var. Selecting three pokemon figure. Seting the names, height and types of pokemons.
+then build the repository*/
+
+
+var repository = [{ name: "Bulbasaur", 
+height: 0.7, 
+types: [ "Grass", "Poison" ]},
+
+{ name: "Venusaur",
+height: 2.0, 
+types: [ "Grass", "Poison" ]},
+
+{ name: "Butterfree",
+height: 1.1, 
+types: [ "Bug", "Flying" ]}]
+
+var pokemon = respository[i];
+
+/* Using what learned to make the loop and testing if it's working properly.*/
+
+for (var i = 0; i < repository.length; i++) {
+        
+        document.write("pokemon.name: ", i, repository[i])
+        
+        if (repository[i].height === 2.0) {
+                document.write("<p>" +pokemon.name+"This one is really huge!" +"</p>");
+                }
+              
+                else {
+                document.write("<p>Here they are!</p>");
+                }
+}
