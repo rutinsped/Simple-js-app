eda99231f1602669d649aec2269d0ee57f7ab912
rutinsped <rutinsped1@gmail.com>
2019-05-24 17:12:09 +0200

    Wraping everything in two IIFE to protect the codes and make a pattern.

--- appjs/scripts.js
+++ appjs/scripts.js
@@ -1,25 +1,48 @@
-/* Seting the repository for var. Selecting three pokemon figure. Seting the names, height and types of pokemons.
-then build the repository*/
-
-
-var repository = [{ name: "Bulbasaur", 
-height: 0.7, 
-types: [ "Grass", "Poison" ]},
-
-{ name: "Venusaur",
-height: 2.0, 
-types: [ "Grass", "Poison" ]},
-
-{ name: "Butterfree",
-height: 1.1, 
-types: [ "Bug", "Flying" ]}]
-
-repository.forEach(function(property){
-    if (property.height >= 2.0) {
-              document.write("<p>" +property.name+" "+property.height+ " This one is really huge!" +"</p>");
-          }
-          else {
-          document.write("<p>" +property.name+" "+property.height+ "</p>");
-          }
-  });
-  
+(function (){pokemonRepository = (function ()
+    {
+    var repository = [{ name: "Bulbasaur",
+    height: 0.7,
+    types: [ "Grass", "Poison" ]},
+    
+    { name: "Venusaur",
+    height: 2.0,
+    types: [ "Grass", "Poison" ]},
+    
+    { name: "Butterfree",
+    height: 1.1,
+    types: [ "Bug", "Flying" ]}]
+    
+    
+    function add(pokemon) {
+      repository.push(pokemon);
+    }
+    
+    function getAll() {
+      return repository;
+    }
+    
+    return {
+      add: add,
+      getAll: getAll
+    }
+    })();
+    
+    var pokemon = { name: "Arbok",
+    height: 3.5,
+    types: [ "Poison" ]};
+    var getAll= pokemonRepository.getAll();
+    
+    pokemonRepository.add(pokemon)
+    
+    getAll.forEach(function(property){
+      if (property.height >= 2.0) {
+                document.write("<p>" +property.name+" "+property.height+ " This one is really huge!" +"</p>");
+            }
+            else {
+            document.write("<p>" +property.name+" "+property.height+ "</p>");
+            }
+             });
+    
+    pokemonRepository();
+    })();
+    
