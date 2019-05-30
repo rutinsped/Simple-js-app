b4449b552bc94bb55ebc9c3a055c992c3caed58d
rutinsped <rutinsped1@gmail.com>
2019-05-30 16:06:58 +0200

    Create new list elements with buttons and display them on HTML site

--- appjs/scripts.js
+++ appjs/scripts.js
@@ -1,48 +1,68 @@
 (function (){pokemonRepository = (function ()
-    {
-    var repository = [{ name: "Bulbasaur",
-    height: 0.7,
-    types: [ "Grass", "Poison" ]},
-    
-    { name: "Venusaur",
-    height: 2.0,
-    types: [ "Grass", "Poison" ]},
-    
-    { name: "Butterfree",
-    height: 1.1,
-    types: [ "Bug", "Flying" ]}]
-    
-    
-    function add(pokemon) {
-      repository.push(pokemon);
-    }
-    
-    function getAll() {
-      return repository;
-    }
-    
-    return {
-      add: add,
-      getAll: getAll
-    }
-    })();
-    
-    var pokemon = { name: "Arbok",
-    height: 3.5,
-    types: [ "Poison" ]};
-    var getAll= pokemonRepository.getAll();
-    
-    pokemonRepository.add(pokemon)
-    
-    getAll.forEach(function(property){
-      if (property.height >= 2.0) {
-                document.write("<p>" +property.name+" "+property.height+ " This one is really huge!" +"</p>");
-            }
-            else {
-            document.write("<p>" +property.name+" "+property.height+ "</p>");
-            }
-             });
-    
-    pokemonRepository();
-    })();
-    
+  {
+  var repository = [{ name: "Bulbasaur",
+  height: 0.7,
+  types: [ "Grass", "Poison" ]},
+ 
+  { name: "Venusaur",
+  height: 2.0,
+  types: [ "Grass", "Poison" ]},
+ 
+  { name: "Butterfree",
+  height: 1.1,
+  types: [ "Bug", "Flying" ]},
+
+  { name: "Poliwrath",
+  height: 1.3,
+  types: [ "Water", "Fighting" ]},
+
+  { name: "Nidoking",
+  height: 1.4,
+  types: [ "Poison", "Ground" ]}]
+
+  function add(pokemon) {
+    repository.push(pokemon);
+  }
+ 
+  function getAll() {
+    return repository;
+  }
+ 
+  return {
+    add: add,
+    getAll: getAll
+  }
+  })();
+ 
+  var pokemon = { name: "Arbok",
+  height: 3.5,
+  types: [ "Poison" ]};
+ 
+  var getAll= pokemonRepository.getAll();
+  var $pokelist = document.querySelector('.pokelist');
+ 
+  function showDetails(pokemon) {
+    var pokemon = pokemon.name;
+   }
+ 
+  function addListItem(pokelist) {
+    var pokemon = pokelist;
+    var listItemElement = document.createElement('li');
+    var btn = document.createElement("BUTTON");
+    btn.innerText = pokemon.name;
+ 
+    listItemElement.innerText = pokemon.name;
+    listItemElement.appendChild(btn);
+    btn.addEventListener ("click", function() {
+      showDetails(pokemon);
+        });
+    $pokelist.appendChild(listItemElement);
+  }
+ 
+ 
+  pokemonRepository.add(pokemon)
+ 
+  getAll.forEach(addListItem);
+ 
+ 
+  })();
