38200d85bd883bc965eecf1a6e5c48a63f6140ae
rutinsped <rutinsped1@gmail.com>
2019-06-06 08:05:19 +0200

    Make function loadList

--- appjs/scripts.js
+++ appjs/scripts.js
@@ -1,52 +1,70 @@
-(function (){pokemonRepository = (function ()
-  {
-  var repository = [{ name: "Bulbasaur",
-  height: 0.7,
-  types: [ "Grass", "Poison" ]},
- 
-  { name: "Venusaur",
-  height: 2.0,
-  types: [ "Grass", "Poison" ]},
- 
-  { name: "Butterfree",
-  height: 1.1,
-  types: [ "Bug", "Flying" ]},
+(function (){
+  
+  pokemonRepository = (function (){
+    var repository = []
+  
+    var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  
+    function add(pokemon) {
+      repository.push(pokemon);
+    }
+  
+    function getAll() {
+      return repository;
+    }
+  
+    function loadList() {
+      return fetch(apiUrl).then(function (response) {
+        return response.json();
+      }).then(function (json) {
+        json.results.forEach(function (item) {
+          var pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+          };
+          add(pokemon);
+        });
+      }).catch(function (e) {
+        console.error(e);
+      })
+    }
+  
+    function loadDetails(pokemon) {
+      var url = item.detailsUrl;
+      return fetch(url)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (details) {
+          pokemon.imageUrl = details.sprites.front_default;
+          pokemon.height = details.height;
+          pokemon.types = Object.keys(details.types);
+        })
+        .catch(function (e) {
+          console.error(e);
+        });
+    }
+  
+    return {
+      add: add,
+      getAll: getAll,
+      loadList: loadList,
+      loadDetails: loadDetails,
+      //showDetails: showDetails
+    }
 
-  { name: "Poliwrath",
-  height: 1.3,
-  types: [ "Water", "Fighting" ]},
-
-  { name: "Nidoking",
-  height: 1.4,
-  types: [ "Poison", "Ground" ]}]
-
-  function add(pokemon) {
-    repository.push(pokemon);
-  }
- 
-  function getAll() {
-    return repository;
-  }
- 
-  return {
-    add: add,
-    getAll: getAll
-  }
   })();
- 
-  var pokemon = { name: "Arbok",
-  height: 3.5,
-  types: [ "Poison" ]};
- 
-  var getAll= pokemonRepository.getAll();
+
+
   var $pokelist = document.querySelector('.pokelist');
  
   function showDetails(pokemon) {
-    var pokemon = pokemon.name;
-    console.log(pokemon, name);
-   }
+
+    console.log(pokemon.name, pokemon.detailsUrl);
+  }
  
   function addListItem(pokelist) {
+    
     var pokemon = pokelist;
     var listItemElement = document.createElement('li');
     var btn = document.createElement("BUTTON");
@@ -54,16 +72,28 @@
  
     listItemElement.innerText = pokemon.name;
     listItemElement.appendChild(btn);
+
     btn.addEventListener ("click", function() {
       showDetails(pokemon);
-        });
+    });
+ 
     $pokelist.appendChild(listItemElement);
   }
+  
+ 
+ // pokemonRepository.add(pokemon)
+ 
+ // Calls the loadList function and then call the getAll function
+ // to get all pokemons in the repository array inside the
+ // pokemonRepository IIFE
+
+  pokemonRepository.loadList().then(function() {
+
+    var getAll = pokemonRepository.getAll();
+    getAll.forEach(addListItem);
+    
+  });
  
  
-  pokemonRepository.add(pokemon)
+ })();
  
-  getAll.forEach(addListItem);
- 
- 
-  })();
